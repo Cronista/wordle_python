f42335c958be0b21ddf36174b3db919a554198cf
Cronista <whamack@live.com>
2023-02-06 20:33:31 +0000

    pygame and ui initialization

--- wordly.py
+++ wordly.py
@@ -1,16 +1,39 @@
 import pygame
-#import pygame_gui
+##import pygame_gui
 
 pygame.init()
 
-pygame.display.set_caption('Quick Start')
-window_surface = pygame.display.set_mode((800, 600))
+#define variables
+width = 800
+height = 600
+yellow = (204,204, 0)
+green = (0, 255, 0)
+black = (0, 0, 0)
+gray = (166, 166, 166)
 
-background = pygame.Surface((800, 600))
+#define gui; create canvas
+pygame.display.set_caption('Jogo de Termos')
+canvas = pygame.display.set_mode((width, height))
+
+background = pygame.Surface((width, height))
 background.fill(pygame.Color('#000000'))
 
-#manager = pygame_gui.UIManager((800, 600))
+##manager = pygame_gui.UIManager((800, 600))
 
+#draw background into canvas
+canvas.blit(background, (0,0))
+
+#draw word/letters container 
+pygame.draw.rect(canvas, gray, (230,50,700,140), 5)
+
+#update display
+pygame.display.update()
+
+#main logic (with placeholders)
+
+
+
+#exit routine
 is_running = True
 
 while is_running:
@@ -18,7 +41,3 @@ while is_running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
-
-window_surface.blit(background, (0,0))
-
-pygame.display.update()
